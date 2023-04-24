25466f582b68d240638cce3253e8d65995e5ad49
2md-mahmudul-hasan <2md.mahmudul.hasan@gamil.com>
2023-04-24 08:33:37 +0600

    p-setup

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNJw_Kk0jXEJ1DEVSmWtQKdzOyoQbgfx4",
+  authDomain: "daily-start-68ee3.firebaseapp.com",
+  projectId: "daily-start-68ee3",
+  storageBucket: "daily-start-68ee3.appspot.com",
+  messagingSenderId: "158789964349",
+  appId: "1:158789964349:web:7d1f018c2e2254852e9054"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
